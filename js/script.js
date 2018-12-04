b53594253d8c76284ad2c4c204b6d188310219db
Konrad Sedkowski <ksedkowski@gmail.com>
2018-12-04 10:32:59 +0100

    fixed

--- js/script.js
+++ js/script.js
@@ -21,49 +21,20 @@ var Counter = React.createClass({
         return (
             React.createElement('div', {}, 
                 React.createElement('button', {onClick: this.increment}, 'dodaj'),
-                React.createElement('span', {}, 'Licznik_1 ' + this.state.counter),
+                React.createElement('span', {}, 'Licznik ' + this.state.counter),
                 React.createElement('button', {onClick: this.decrement}, 'odemij')
             )
         );
     }
 });
 
-var Counter_2 = React.createClass({
-    getInitialState: function() {
-        return {
-            counter: 0,            
-        };
-    },
-
-    increment: function() {
-        this.setState({
-            counter: this.state.counter + 1
-        });
-    },
-
-    decrement: function() {
-        this.setState({
-            counter: this.state.counter - 1
-        });
-    },
-
-    render: function() {
-        return (
-            React.createElement('div', {}, 
-                React.createElement('button', {onClick: this.increment}, 'dodaj'),
-                React.createElement('span', {}, 'Licznik_2 ' + this.state.counter),
-                React.createElement('button', {onClick: this.decrement}, 'odemij')
-            )
-        );
-    }
-}); 
-
 var Element = React.createClass({
   render: function(){
     return (
         React.createElement('div', {},
-            React.createElement(Counter, {}, ),
-            React.createElement(Counter_2, {},)
+            React.createElement(Counter, {},),
+            React.createElement(Counter, {},),
+            React.createElement(Counter, {},)
         )      
     );
   }
